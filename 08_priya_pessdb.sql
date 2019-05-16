c1962f38fee34c406abea70bd9b9e951ee8a18aa
Mangalapriya <50565037+Mangalapriya@users.noreply.github.com>
2019-05-16 15:38:10 +0800

    Add files via upload

---
+++ 08_priya_pessdb.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2019 at 09:18 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `08_priya_pessdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dispatch`
+--
+
+CREATE TABLE `dispatch` (
+  `incident_id` int(11) NOT NULL,
+  `patrolcar_id` varchar(10) NOT NULL,
+  `time_dispatched` datetime NOT NULL,
+  `time_arrived` datetime DEFAULT NULL,
+  `time_completed` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident`
+--
+
+CREATE TABLE `incident` (
+  `incident_id` varchar(11) NOT NULL,
+  `caller_number` varchar(30) NOT NULL,
+  `phone_Number` varchar(10) NOT NULL,
+  `incident_Type_ld` varchar(3) NOT NULL,
+  `incident_Location` varchar(50) NOT NULL,
+  `incident_Desc` varchar(100) NOT NULL,
+  `incident_Status_id` varchar(1) NOT NULL,
+  `time_Called` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_status`
+--
+
+CREATE TABLE `incident_status` (
+  `incident_status_id` varchar(1) NOT NULL,
+  `incident_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_type`
+--
+
+CREATE TABLE `incident_type` (
+  `incident_type_id` varchar(3) NOT NULL,
+  `incident_type_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar`
+--
+
+CREATE TABLE `patrolcar` (
+  `patrolcar_id` varchar(10) NOT NULL,
+  `patrolcar_status_id` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar_status`
+--
+
+CREATE TABLE `patrolcar_status` (
+  `patrolcar_status_id` varchar(1) NOT NULL,
+  `patrolcar_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dispatch`
+--
+ALTER TABLE `dispatch`
+  ADD PRIMARY KEY (`incident_id`,`patrolcar_id`);
+
+--
+-- Indexes for table `incident`
+--
+ALTER TABLE `incident`
+  ADD PRIMARY KEY (`incident_id`);
+
+--
+-- Indexes for table `incident_status`
+--
+ALTER TABLE `incident_status`
+  ADD PRIMARY KEY (`incident_status_id`);
+
+--
+-- Indexes for table `incident_type`
+--
+ALTER TABLE `incident_type`
+  ADD PRIMARY KEY (`incident_type_id`);
+
+--
+-- Indexes for table `patrolcar_status`
+--
+ALTER TABLE `patrolcar_status`
+  ADD PRIMARY KEY (`patrolcar_status_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
